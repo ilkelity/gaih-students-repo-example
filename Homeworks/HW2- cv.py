0664c2408844b6b62b3bb12dc41ccc35a41b671b
ilke akar <75377034+ilkelity@users.noreply.github.com>
2021-03-14 20:05:38 +0300

    Add files via upload

---
+++ Homeworks/HW2- cv.py
@@ -0,0 +1,18 @@
+# CV
+
+cv_first = {"name": "John", "surname": "Smith", "age": 24, "speciality": "Administrative assistant",
+            "skills": "problem solving, adaptability,strong work ethic"}
+cv_second = {"name": "Angela", "surname": "Wilkinson", "age": 28, "speciality": "Family medicine",
+             "skills": "good communication skills, planning and organisation"}
+cv_third = {"name": "David", "surname": "Williams", "age": 26, "speciality": "Financial Advisor",
+            "skills": "analytical skills, excellent communication,business and finance skills"}
+cv_fourth = {"name": "Susan", "surname": "Jones", "age": 30, "speciality": "Software Developer",
+             "skills": "Mathematical aptitude, Problem-solving skills, Accuracy and attention to detail"}
+cv_fifth = {"name": "Frank", "surname": "Hall", "age": 35, "speciality": "Epidemiologist",
+            "skills": "Critical-thinking, Teaching skills"}
+
+print(cv_first)
+print(cv_second)
+print(cv_third)
+print(cv_fourth)
+print(cv_fifth)
